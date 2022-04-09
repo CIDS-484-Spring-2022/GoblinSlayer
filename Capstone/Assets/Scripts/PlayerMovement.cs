83167c0d836bc25ea082e94c226e5b43ed3a686f
Shiiroyashaaa <stevenvang2011@hotmail.com>
2022-04-09 17:32:27 -0500

    Updated script to allow flipping

--- Capstone/Assets/Scripts/PlayerMovement.cs
+++ Capstone/Assets/Scripts/PlayerMovement.cs
@@ -18,7 +18,51 @@ public class PlayerMovement : MonoBehaviour
 
         transform.Translate(moveHorizontal, moveVertical, 0);
 
+        Vector3 characterScale = transform.localScale;
+        /*
+        if (Input.GetKey(KeyCode.A)){
+            GetComponent<Transform>().eulerAngles = new Vector3 (0, 0, -90);
+        }
+        if (Input.GetKey(KeyCode.D)){
+            GetComponent<Transform>().eulerAngles = new Vector3 (0, 0, 90);
+        }
+        if (Input.GetKey(KeyCode.W)){
+            GetComponent<Transform>().eulerAngles = new Vector3 (0, 0, 180);
+        }
+        if (Input.GetKey(KeyCode.S)){
+            GetComponent<Transform>().eulerAngles = new Vector3 (0, 0, 0);
+        }
+        */
+
+        // Character flips on x-axis
+        if(Input.GetAxis("Horizontal") < 0 ){
+            /*
+            Transform transform = GetComponent<Transform>();
+            transform.Rotate( new Vector3(0, 0, 90));
+            */
+            //GetComponent<Transform>().eulerAngles = new Vector3 (0, 0, -90);
+            characterScale.x = -3;
+            
+        }
+        if(Input.GetAxis("Horizontal") > 0 ){
+            characterScale.x = 3;
+            //GetComponent<Transform>().eulerAngles = new Vector3 (0, 0, 90);
+        }
         
+        //Character flips on y-axis
+        if(Input.GetAxis("Vertical") < 0 ){
+            characterScale.y = 3;
+            //GetComponent<Transform>().eulerAngles = new Vector3 (0, 0, 180);
+        }
+        
+        if(Input.GetAxis("Vertical") > 0 ){
+            characterScale.y = -3;
+            //GetComponent<Transform>().eulerAngles = new Vector3 (0, 0, 0);
+        }
+        
+        transform.localScale = characterScale;
+        
+
     }
 
 }
